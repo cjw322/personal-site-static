620f98cd595d316b416ccb759033ff3d5e53317a
Cora Wu <cjw322@cornell.edu>
2020-03-22 17:13:42 -0700

    added js to redirect users to current site

---
+++ redirect.js
@@ -0,0 +1,7 @@
+// alert("Working")
+var text = "This is an old version of this personal website. Do you want to visit the current website instead?"
+var reroute = window.confirm(text);
+
+if (reroute) {
+  window.location.href = "http://corawu22.com/";
+}
